2f90a938500b50465f3cd5d5754e5a5d82c0d1eb
rafa1um <rafael.rxp1@gmail.com>
2019-06-10 09:02:15 -0300

    Shitnished

---
+++ reservas/drop_reserplane.sql
@@ -0,0 +1,12 @@
+DROP TABLE res_trch;
+DROP TABLE reserva;
+DROP TABLE horario;
+DROP TABLE trecho;
+DROP TABLE voo;
+DROP TABLE aeroporto;
+DROP TABLE cidade;
+DROP TABLE tipo_aeronave_assento;
+DROP TABLE tipo_aeronave;
+DROP TABLE assento;
+
+
